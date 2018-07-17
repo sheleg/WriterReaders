daf85a1bab4afb6f36952db85c10947a3ad54cb7
sheleg <uladzislava.sheleh@solarwinds.com>
2018-07-17 17:48:18 +0300

    Clean version

--- RWL.cpp
+++ RWL.cpp
@@ -6,36 +6,16 @@ std::mutex cout_mutex;
 
 void RWL::start_read()
 {
-	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "Enter function start read " << std::this_thread::get_id() << std::endl;
-	}
-
 	std::unique_lock<std::mutex> u_lock(mutex_);
 	reader_cv_.wait(u_lock, [this]() { return writer_count_ == 0 && !is_writer_active_; });
-		++reader_count_;
-
-	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "START READ " << std::this_thread::get_id() << std::endl;
-	}
+	++reader_count_;
 }
 
 void RWL::stop_read()
 {
 	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "Enter function stop read " << std::this_thread::get_id() << std::endl;
-	}
-
-	{
 		std::lock_guard<std::mutex> l_guard(mutex_);
 		reader_count_--;
-	}
-
-	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "END READ " << std::this_thread::get_id() << std::endl;
 	}
 
 	writer_cv_.notify_one();
@@ -43,20 +23,10 @@ void RWL::stop_read()
 
 void RWL::start_write()
 {
-	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "Enter function start write " << std::this_thread::get_id() << std::endl;
-	}
-
 	std::unique_lock<std::mutex> u_lock(mutex_);
 	++writer_count_;
 	writer_cv_.wait(u_lock, [this]() { return reader_count_ == 0 && !is_writer_active_; });
 	u_lock.unlock();
-
-	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "START WRITE " << std::this_thread::get_id() << std::endl;
-	}
 
 	mutex_.lock();
 	is_writer_active_ = true;
@@ -65,11 +35,6 @@ void RWL::start_write()
 
 void RWL::stop_write()
 {
-	{
-		std::lock_guard<std::mutex> l_guard(cout_mutex);
-		std::cout << "END WRITE " << std::this_thread::get_id() << std::endl;
-	}
-
 	is_writer_active_ = false;
 	mutex_.unlock();
 
